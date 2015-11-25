e11f36e587434772d2a39c2270d30229dc966079
uLiL <luri@kioss.com>
2015-11-25 11:51:13 +0700

    init newrelic integration

---
+++ FastPlaz/src/integration/newrelic_integration.pas
@@ -0,0 +1,22 @@
+unit newrelic_integration;
+
+{$mode objfpc}{$H+}
+{$include ../../define.inc}
+
+interface
+
+uses
+  http_lib,
+  Classes, SysUtils;
+
+{$ifdef NEWRELIC_INTEGRATION}
+{$endif}
+
+implementation
+
+{$ifdef NEWRELIC_INTEGRATION}
+
+{$endif}
+
+end.
+
