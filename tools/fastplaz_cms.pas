e3622d72406c632adbb8567b2059f2b4113b77dc
uLiL <luri@kioss.com>
2016-07-26 20:17:54 +0700

    fix kecil kecil

--- tools/fastplaz_cms.pas
+++ tools/fastplaz_cms.pas
@@ -7,9 +7,9 @@ unit fastplaz_cms;
 interface
 
 uses
-  admin_controller, groupadmin_controller, permissionadmin_controller, 
-  search_controller, systeminfo_controller, user_controller, 
-  useradmin_controller, group_model, groupmembership_model, 
+  admin_controller, groupadmin_controller, notification_controller, 
+  permissionadmin_controller, search_controller, systeminfo_controller, 
+  user_controller, useradmin_controller, group_model, groupmembership_model, 
   grouppermission_model, user_model, group_util, permission_util, 
   security_util, user_util, hash_tools;
 
