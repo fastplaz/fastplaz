daaf82e74a4996c57fb05cebc82e932e2438e9c5
uLiL <luri@kioss.com>
2017-01-16 23:30:31 +0700

    whois integration

---
+++ FastPlaz/src/integration/whois_integration.pas
@@ -0,0 +1,261 @@
+unit whois_integration;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  cthreads, Sockets, RegExpr,
+  Classes, SysUtils;
+
+const
+  WHOIS_DEFAULT_SERVER = '199.7.52.74'; // whois.internic.net
+  WHOIS_DEFAULT_PORT = 43;
+  WHOIS_DEFAULT_TIMEOUT = 2500;
+
+  _WHOIS_REGEX_REGISTRAR = 'Registrar: ([a-zA-Z0-9\ ,.\-]+)';
+  _WHOIS_REGEX_NAMESERVER = 'Name Server: ([a-zA-Z0-9.\-]+)';
+  _WHOIS_REGEX_STATUS = 'Status: ([a-zA-Z0-9]+)';
+  _WHOIS_REGEX_UPDATEDDATE = 'Updated Date: ([a-zA-Z0-9\ \-,.]+)';
+  _WHOIS_REGEX_CREATEDDATE = 'Creation Date: ([a-zA-Z0-9\ \-,.]+)';
+  _WHOIS_REGEX_EXPIREDDATE = 'Expiration Date: ([a-zA-Z0-9\ \-,.]+)';
+
+type
+
+  { TWhoisIntegration }
+
+  TWhoisIntegration = class(TInterfacedObject)
+  private
+    regex: TRegExpr;
+    FData: TStringList;
+    FisConnected: boolean;
+    FLastError: integer;
+    FLastMessage: string;
+    FPort: string;
+    FPortInt: integer;
+    FServer: string;
+
+    sock: TInetSockAddr;
+    sockInt: longint;
+    SocketIn, SocketOut: Text;
+    IpAddress: in_addr;
+
+    function getCreateDate: string;
+    function getExpiredDate: string;
+    function getNameServer: string;
+    function getRegistrar: string;
+    function getStatus: string;
+    function getUpdatedDate: string;
+    procedure setPort(AValue: string);
+    procedure setServerAddress(AValue: string);
+
+    function Connect: boolean;
+    function sendString(Strings: string): boolean;
+  public
+    constructor Create; virtual;
+    destructor Destroy; virtual;
+    function Find(DomainName: string): boolean;
+
+    property Server: string read FServer write setServerAddress;
+    property Port: string read FPort write setPort;
+    property Data: TStringList read FData write FData;
+
+    property Registrar: string read getRegistrar;
+    property NameServer: string read getNameServer;
+    property Status: string read getStatus;
+    property CreationDate: string read getCreateDate;
+    property ExpiredDate: string read getExpiredDate;
+    property UpdatedDate: string read getUpdatedDate;
+
+    property isConnected: boolean read FisConnected;
+    property LastError: integer read FLastError;
+    property LastMessage: string read FLastMessage;
+  end;
+
+implementation
+
+{ TWhoisIntegration }
+
+procedure TWhoisIntegration.setPort(AValue: string);
+begin
+  if FPort = AValue then
+    Exit;
+  try
+    FPortInt := StrToInt(AValue);
+    FPort := AValue;
+  except
+    FPort := IntToStr(WHOIS_DEFAULT_PORT);
+    FPortInt := WHOIS_DEFAULT_PORT;
+  end;
+end;
+
+function TWhoisIntegration.getCreateDate: string;
+begin
+  Result := '-';
+  regex.Expression := _WHOIS_REGEX_CREATEDDATE;
+  if regex.Exec(FData.Text) then
+    Result := regex.Match[1];
+end;
+
+function TWhoisIntegration.getExpiredDate: string;
+begin
+  Result := '-';
+  regex.Expression := _WHOIS_REGEX_EXPIREDDATE;
+  if regex.Exec(FData.Text) then
+    Result := regex.Match[1];
+end;
+
+function TWhoisIntegration.getNameServer: string;
+var
+  i: integer;
+begin
+  Result := '-';
+  regex.Expression := _WHOIS_REGEX_NAMESERVER;
+  if regex.Exec(FData.Text) then
+  begin
+    i := 1;
+    Result := '';
+    repeat
+      Result := regex.Match[i] + ' ';
+      Inc(i);
+    until regex.Match[i] = '';
+  end;
+end;
+
+function TWhoisIntegration.getRegistrar: string;
+begin
+  Result := '-';
+  regex.Expression := _WHOIS_REGEX_REGISTRAR;
+  if regex.Exec(FData.Text) then
+    Result := regex.Match[1];
+end;
+
+function TWhoisIntegration.getStatus: string;
+begin
+  Result := '-';
+  regex.Expression := _WHOIS_REGEX_STATUS;
+  if regex.Exec(FData.Text) then
+    Result := regex.Match[1];
+end;
+
+function TWhoisIntegration.getUpdatedDate: string;
+begin
+  Result := '-';
+  regex.Expression := _WHOIS_REGEX_UPDATEDDATE;
+  if regex.Exec(FData.Text) then
+    Result := regex.Match[1];
+end;
+
+procedure TWhoisIntegration.setServerAddress(AValue: string);
+begin
+  if FServer = AValue then
+    Exit;
+  FServer := AValue;
+  IpAddress := StrToNetAddr(AValue);
+  if IpAddress.s_addr = 0 then
+  begin
+    FServer := WHOIS_DEFAULT_SERVER;
+    IpAddress := StrToNetAddr(FServer);
+  end;
+end;
+
+function TWhoisIntegration.Connect: boolean;
+begin
+  Result := FisConnected;
+  FLastMessage := '';
+  FLastError := 1;
+  if FisConnected then
+    exit;
+
+  try
+    IpAddress := StrToNetAddr(FServer);
+    sockInt := fpSocket(AF_INET, SOCK_STREAM, 0);
+    if sockInt = -1 then
+      Exit;
+    sock.sin_family := AF_INET;
+    sock.sin_port := htons(FPortInt);
+    sock.sin_addr.s_addr := IpAddress.s_addr;
+    if Sockets.Connect(sockInt, sock, SocketIn, SocketOut) then
+    begin
+      FisConnected := True;
+      Result := True;
+      FLastError := 0;
+    end;
+  except
+    on E: Exception do
+    begin
+      FLastMessage := E.Message;
+    end;
+  end;
+
+end;
+
+function TWhoisIntegration.sendString(Strings: string): boolean;
+var
+  s: string;
+begin
+  Result := False;
+  FLastError := 0;
+  FLastMessage := 'try..';
+  if not FisConnected then
+  begin
+    if not Connect then
+      Exit;
+  end;
+
+  try
+    Data.Clear;
+    Reset(SocketIn);
+    ReWrite(SocketOut);
+    WriteLn(SocketOut, Strings + #13);
+    Flush(SocketOut);
+    //ReadLn(SocketIn, FLastMessage);
+    //ReadLn(SocketIn, s);
+    //FLastMessage := 'x:' + FLastMessage + '====/====' + s;
+    while not EOF(SocketIn) do
+    begin
+      ReadLn(SocketIn, s);
+      Data.Add(s);
+    end;
+    FLastMessage := 'Ok';
+    Result := True;
+  except
+    on E: Exception do
+    begin
+      FLastMessage := E.Message;
+    end;
+  end;
+
+end;
+
+constructor TWhoisIntegration.Create;
+begin
+  FServer := WHOIS_DEFAULT_SERVER;
+  FPort := IntToStr(WHOIS_DEFAULT_PORT);
+  FPortInt := WHOIS_DEFAULT_PORT;
+  FLastError := 0;
+  FLastMessage := '';
+  FisConnected := False;
+
+  regex := TRegExpr.Create;
+  FData := TStringList.Create;
+  FData.Delimiter := ':';
+end;
+
+destructor TWhoisIntegration.Destroy;
+begin
+  FData.Free;
+  regex.Free;
+end;
+
+function TWhoisIntegration.Find(DomainName: string): boolean;
+begin
+  Result := False;
+  if sendString(DomainName) then
+  begin
+
+    Result := True;
+  end;
+end;
+
+end.
