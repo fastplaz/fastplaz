b649c9f6f931d111e2489da6a44a60cf0cc7905e
uLiL <luri@kioss.com>
2017-01-21 21:43:36 +0700

    kamus kbbi integration

--- FastPlaz/src/integration/whois_integration.pas
+++ FastPlaz/src/integration/whois_integration.pas
@@ -1,3 +1,10 @@
+{
+This file is part of the SimpleBOT package.
+(c) Luri Darmawan <luri@fastplaz.com>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+}
 unit whois_integration;
 
 {
