53f1e765008ee64f37225d3534586dd23e1859c8
Luri <luri@kioss.com>
2018-06-28 12:01:53 +0700

    Similiraty Word: load stopwords feature

--- FastPlaz/src/library/nlp_lib.pas
+++ FastPlaz/src/library/nlp_lib.pas
@@ -5,8 +5,10 @@ unit nlp_lib;
 
   [x] Sentences Similiraty
   NLP := TNLP.Create;
+  NLP.StopWords.LoadFromFile('your/stopword_file');
   Score := NLP.Similarity('your sentences', 'reference sentences');
   .
+  . Score is between 0 and 1
   .
   NLP.Free;
 
@@ -26,41 +28,45 @@ uses
 
 type
 
-  TNLPSimilarityMethod = (smNone, smCosinus);
+  TNLPSimilarityMethod = (smNone, smCosine);
 
   { TNLP }
 
   TNLP = class(TInterfacedObject)
   private
+    FStopWords: TStringList;
+    function similarityCosine(AText, AReference: string): double;
   public
     constructor Create;
     destructor Destroy;
     function Similarity(AText, AReference: string;
-      AMethod: TNLPSimilarityMethod = smCosinus): double;
+      AMethod: TNLPSimilarityMethod = smCosine): double;
     function IsStopWords(AWord: string): boolean;
   published
+    property StopWords: TStringList read FStopWords write FStopWords;
   end;
 
 implementation
 
 const
-  STOPWORDS: array[0..3] of string =
+  STOPWORDS_LOCAL: array[0..5] of string =
     (
-    'di', 'yang', 'ke', 'pada'
+    'di', 'yang', 'ke', 'pada', 'jika', 'seperti'
     );
 
 { TNLP }
 
 constructor TNLP.Create;
 begin
+  FStopWords := TStringList.Create;
 end;
 
 destructor TNLP.Destroy;
 begin
+  FStopWords.Free;
 end;
 
-function TNLP.Similarity(AText, AReference: string;
-  AMethod: TNLPSimilarityMethod): double;
+function TNLP.similarityCosine(AText, AReference: string): double;
 var
   i: integer;
   LOutput: double;
@@ -69,8 +75,13 @@ var
 begin
   Result := 0;
   LOutput := 0;
+
   LText := LowerCase(AText);
   LReference := LowerCase(AReference);
+
+  LText := preg_replace('[^a-z ]', '', LText);
+  LReference := preg_replace('[^a-z ]', '', LReference);
+
   LTextA := Explode(LText, ' ');
   LReferenceA := Explode(LReference, ' ');
   for i := 0 to LTextA.Count - 1 do
@@ -88,10 +99,20 @@ begin
   LTextA.Free;
 end;
 
+function TNLP.Similarity(AText, AReference: string;
+  AMethod: TNLPSimilarityMethod): double;
+begin
+  Result := 0;
+  if AMethod = smCosine then
+    Result := similarityCosine(AText, AReference);
+end;
+
 function TNLP.IsStopWords(AWord: string): boolean;
 begin
   Result := False;
-  if StrInArray(AWord, STOPWORDS) then
+  if StrInArray(AWord, STOPWORDS_LOCAL) then
+    Result := True;
+  if FStopWords.IndexOf(AWord) <> -1 then
     Result := True;
 end;
 
