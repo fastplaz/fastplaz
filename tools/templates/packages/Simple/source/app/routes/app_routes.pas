2c6f91074b2998e2a4e4a0f8738497078f452406
Luri Darmawan <luri@kioss.com>
2022-05-07 18:18:37 +0700

    template: foreach example (json)

--- tools/templates/packages/Simple/source/app/routes/app_routes.pas
+++ tools/templates/packages/Simple/source/app/routes/app_routes.pas
@@ -10,9 +10,10 @@ uses
 implementation
 
 uses info_controller, app_controller, example_controller,
-  database_controller, example_form_controller;
+  database_controller, example_form_controller, example_foreach_controller;
 
 initialization
+  Route[ '/example/foreach'] := TForeachController;
   Route[ '/example/form'] := TFormController;
   Route[ '/example'] := TExampleController;
   Route[ '/database'] := TDatabaseController;
