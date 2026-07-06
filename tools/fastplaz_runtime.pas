7022aa6f3fd98c408d3906c0a4c1f85d9bda6e0a
luridarmawan <luri@carik.id>
2026-07-06 15:43:21 +0700

    refactor(lpk): convert fastplaz_runtime to RunTimeOnly and add regex/thread units
    - Change package Type from RunAndDesignTime to RunTimeOnly
    - Add regexpr_compilers.inc, regexpr_lib, regexpr_unicodedata_lib, thread_custom
    - Normalize path delimiters from backslash to forward slash for cross-platform
    - Add explicit Major=0 to Version
    - Sync uses clause in fastplaz_runtime.pas

--- tools/fastplaz_runtime.pas
+++ tools/fastplaz_runtime.pas
@@ -11,22 +11,14 @@ uses
   about_controller, array_helpers, datetime_helpers, integer_helpers, 
   json_helpers, string_helpers, common, config_lib, database_lib, 
   datetime_lib, html_lib, http_lib, image_lib, json_lib, language_lib, 
-  logutil_lib, mailer_lib, math_lib, nlp_lib, recaptcha_lib, rss_lib, 
-  serialize_lib, stemmingnazief_lib, stemmingnaziefredis_lib, 
-  verbal_expressions_lib, versioninfo_lib, modvar_model, session_model, 
-  docs_controller, error_controller, exception_controller, fastplaz_handler, 
-  info_controller, initialize_controller, module_controller, 
-  session_controller, theme_controller, modvar_util, 
-  fastplaz_runtime_register, LazarusPackageIntf;
+  logutil_lib, mailer_lib, math_lib, nlp_lib, recaptcha_lib, regexpr_lib, 
+  regexpr_unicodedata_lib, rss_lib, serialize_lib, stemmingnazief_lib, 
+  stemmingnaziefredis_lib, thread_custom, verbal_expressions_lib, 
+  versioninfo_lib, modvar_model, session_model, docs_controller, 
+  error_controller, exception_controller, fastplaz_handler, info_controller, 
+  initialize_controller, module_controller, session_controller, 
+  theme_controller, modvar_util, fastplaz_runtime_register;
 
 implementation
 
-procedure Register;
-begin
-  RegisterUnit('fastplaz_runtime_register', @fastplaz_runtime_register.Register
-    );
-end;
-
-initialization
-  RegisterPackage('fastplaz_runtime', @Register);
 end.
